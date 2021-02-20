b993a4a487f0adca445b71373ef8c931f5df603a
techieShubham <64989625+techieShubham@users.noreply.github.com>
2021-02-20 22:32:05 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var app = express();
 
+absolutePath = __dirname + "/views/index.html";
+staticPath = __dirname + "/public"
 
+app.use("/public",express.static(staticPath))
 
+app.get("/", 
+function(req, res) {
+  res.sendFile(absolutePath);
+  
+}
+);
 
+app.get("/json",
+function(req,res){
+ if(process.env.MESSAGE_STYLE === "uppercase")
+ {response = "Hello json".toUpperCase()} 
+ else{response = "Hello json" }
 
+ res.json({"message": response});
+});
+
+// console.log("Hello World");
 
 
 
